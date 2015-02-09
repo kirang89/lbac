58778d48deca373868b9dd671911f30ee1bd5062
Kiran Gangadharan <kiran@Kirans-MacBook-Pro.local>
2015-02-10 03:48:58 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#lbac (Let's Build a Compiler)
+
+This repo contains the code I've written while reading "Let's Build a
+Compiler" by Jack Crenshaw.
+
+I've translated the instructions for i386 architecture (Quite poorly
+if I may say so).
+
+This is merely an experimental and educational compiler and way to
+simple for a productional one. It is fun however :)
---
+++ chapter2.pp
@@ -0,0 +1,181 @@
+
+program Cradle;
+
+Uses sysutils;
+
+{ Constant Declarations }
+
+const TAB = ^I;
+
+{ Variable Declarations }
+
+var Look: char;              { Lookahead Character }
+var sp : Integer;
+
+{ Read New Character From Input Stream }
+
+procedure GetChar;
+begin
+   Read(Look);
+end;
+
+{ Report an Error }
+
+procedure Error(s: string);
+begin
+   WriteLn;
+   WriteLn(^G, 'Error: ', s, '.');
+end;
+
+{ Report Error and Halt }
+
+procedure Abort(s: string);
+begin
+   Error(s);
+   Halt;
+end;
+
+{ Report What Was Expected }
+
+procedure Expected(s: string);
+begin
+   Abort(s + ' Expected');
+end;
+
+{ Match a Specific Input Character }
+
+procedure Match(x: char);
+begin
+   if Look = x then GetChar
+   else Expected('''' + x + '''');
+end;
+
+{ Recognize an Alpha Character }
+
+function IsAlpha(c: char): boolean;
+begin
+   IsAlpha := upcase(c) in ['A'..'Z'];
+end;
+
+{ Recognize a Decimal Digit }
+
+function IsDigit(c: char): boolean;
+begin
+   IsDigit := c in ['0'..'9'];
+end;
+
+{ Get an Identifier }
+
+function GetName: char;
+begin
+   if not IsAlpha(Look) then Expected('Name');
+   GetName := UpCase(Look);
+   GetChar;
+end;
+
+{ Get a Number }
+
+function GetNum: char;
+begin
+   if not IsDigit(Look) then Expected('Integer');
+   GetNum := Look;
+   GetChar;
+end;
+
+{ Output a String with Tab }
+
+procedure Emit(s: string);
+begin
+   Write(TAB, s);
+end;
+
+{ Output a String with Tab and CRLF }
+
+procedure EmitLn(s: string);
+begin
+   Emit(s);
+   WriteLn;
+end;
+
+{ Initialize }
+
+procedure Init;
+begin
+   GetChar;
+end;
+
+{ Parse and Translate a Math Factor }
+
+procedure Factor;
+begin
+   EmitLn('movl $' + GetNum + ', %eax')
+end;
+
+{ Recognize and Translate a Multiply }
+
+procedure Multiply;
+begin
+   Match('*');
+   Factor;
+   EmitLn('imul (SP+,D0');
+end;
+
+{ Recognize and Translate a Divide }
+
+procedure Divide;
+begin
+   Match('/');
+   Factor;
+   EmitLn('MOVE (SP)+,D1');
+   EmitLn('DIVS D1,D0');
+end;
+
+{ Recognize a term }
+
+procedure Term;
+begin
+   EmitLn('movl $' + GetNum + ', %eax')
+end;
+
+{ Recognize and Translate an Add }
+
+procedure Add;
+begin
+   Match('+');
+   Term;
+   EmitLn('addl '+ IntToStr(sp) +'(%esp), %eax');
+end;
+
+{ Recognize and Translate a Subtract }
+
+procedure Subtract;
+begin
+   Match('-');
+   Term;
+   EmitLn('subl '+ IntToStr(sp) +'(%esp), %eax');
+   EmitLn('negl %eax')
+end;
+
+{ Parse and Translate a Math Expression }
+
+procedure Expression;
+begin
+   Term;
+   while Look in ['+', '-'] do begin
+      sp := sp - 4;
+      EmitLn('movl %eax, ' + IntToStr(sp) + '(%esp)');
+      case LOOK of
+	'+' : Add;
+	'-' : Subtract;
+      else Expected('Addop');
+      end;
+   end;
+end;
+
+{ Main Program }
+
+begin
+   sp := 0;
+   Init;
+   Expression;
+end.
